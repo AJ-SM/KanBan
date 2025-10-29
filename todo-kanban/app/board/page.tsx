233f070136be68a9bf8837aab1e0c62ad23fc068
AJ-SM <anujsidam1108@gmail.com>
2025-10-29 10:56:16 +0530

    added component

--- todo-kanban/app/board/page.tsx
+++ todo-kanban/app/board/page.tsx
@@ -1,28 +1,34 @@
 
-
+import { Todo } from "@/component/todo"
 
 export default function Board(){
 
     return(
         <div className=" flex justify-center items-center w-screen h-screen bg-slate-900">
             <div className="flex gap-8">
-                <div className="w-80 h-120 bg-slate-800 p-7 rounded-xl">
-                    <div className="font-bold ml-18 text-slate-400 felx justify-center items-center" >BLACKLOG</div>
+                <div className="w-80 h-auto bg-slate-800 p-7 rounded-xl">
+                    <div className=" ml-18 text-slate-400 felx justify-center items-center" >BLACKLOG</div>
 
                 </div>
 
-                <div className="w-80 h-120 bg-slate-800 p-7 rounded-xl">
-                    <div className="font-bold ml-18 text-red-500 felx justify-center items-center" >TODO</div>
+                <div className="w-80 h-auto bg-slate-800 p-7 rounded-xl">
+                    <div className="ml-18 text-red-500 felx justify-center items-center" >TODO</div>
+                    <Todo text="Hellow nigger" type="completed"/>
+                    <Todo text="Hellow nigger" type="inprogress"/>
+                    <Todo text="Hellow nigger" type="backlog"/>
+                    <Todo text="Hellow nigger" type="todo"/>
+                    <Todo text="Hellow nigger"/>
+                    <Todo text="Hellow nigger"/>
 
                 </div>
 
-                <div className="w-80 h-120 bg-slate-800 p-7 rounded-xl">
-                    <div className="font-bold ml-18 text-yellow-300 felx justify-center items-center" >INPROGRESS</div>
+                <div className="w-80 h-auto bg-slate-800 p-7 rounded-xl">
+                    <div className="ml-18 text-yellow-300 felx justify-center items-center" >INPROGRESS</div>
 
                 </div>
 
-                <div className="w-80 h-120 bg-slate-800 p-7 rounded-xl">
-                    <div className="font-bold ml-18 text-green-500 felx justify-center items-center" >COMPLETED</div>
+                <div className="w-80 h-auto bg-slate-800 p-7 rounded-xl">
+                    <div className="ml-18 text-green-500 felx justify-center items-center" >COMPLETED</div>
 
                 </div>
 
---
+++ todo-kanban/component/todo.tsx
@@ -0,0 +1,24 @@
+import { div } from "motion/react-client";
+
+
+interface TodoProp {
+    text:string;
+    type?: 'backlog' | 'todo' | 'inprogress' | 'completed';
+}
+
+
+export function Todo(props:TodoProp){
+    const {text,type} = props
+
+    return(
+        <div draggable='true' className="flex items-center gap-1 mt-3 p-4 bg-slate-700 rounded-xl ">
+            {type== 'backlog' ? <div className="w-3 h-3 rounded-full bg-slate-500"></div> : <div></div>}
+            {type== 'todo' ? <div className="w-3 h-3 rounded-full bg-red-500"></div> : <div></div>}
+            {type == 'inprogress' ? <div className="w-3 h-3 rounded-full bg-yellow-500"></div> : <div></div>}
+            {type == 'completed' ? <div className="w-3 h-3 rounded-full bg-green-500"></div>:<div></div>}
+            {text}
+        </div>
+    )
+}
+
+
