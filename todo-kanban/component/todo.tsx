0c031f9b9a72e3efa48003056944c98ec6e754a4
AJ-SM <anujsidam1108@gmail.com>
2025-10-30 08:42:08 +0530

    added more files

--- todo-kanban/component/todo.tsx
+++ todo-kanban/component/todo.tsx
@@ -1,5 +1,6 @@
-import { div } from "motion/react-client";
+"use client"
 
+import {motion} from "motion/react"
 
 interface TodoProp {
     text:string;
@@ -10,14 +11,21 @@ interface TodoProp {
 export function Todo(props:TodoProp){
     const {text,type} = props
 
+
     return(
-        <div draggable='true' className="flex items-center gap-1 mt-3 p-4 bg-slate-700 rounded-xl ">
+        <motion.div 
+        drag
+        whileHover={{ scale: 1.1 }}
+        transition={{duration:0.3}}
+     
+
+        className="flex items-center hover:cursor-grab active:cursor-grabbing gap-1 mt-3 p-4 bg-slate-700 rounded-xl ">
             {type== 'backlog' ? <div className="w-3 h-3 rounded-full bg-slate-500"></div> : <div></div>}
             {type== 'todo' ? <div className="w-3 h-3 rounded-full bg-red-500"></div> : <div></div>}
             {type == 'inprogress' ? <div className="w-3 h-3 rounded-full bg-yellow-500"></div> : <div></div>}
             {type == 'completed' ? <div className="w-3 h-3 rounded-full bg-green-500"></div>:<div></div>}
             {text}
-        </div>
+        </motion.div>
     )
 }
 
